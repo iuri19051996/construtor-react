6eb7f65dd7c70d4d6462834f8e0ea207cd5aca05
iuri19051996 <iuri.floriano@vitru.com.br>
2024-10-23 16:09:22 -0300

    menu

--- src/componentes/menuLateralHome/menuLateralHome.jsx
+++ src/componentes/menuLateralHome/menuLateralHome.jsx
@@ -1,6 +1,18 @@
 import { Link } from 'react-router-dom';
 import { Box, useColorModeValue } from '@chakra-ui/react';
 import "./menuLateralHome.css"
+import { FaHome } from "react-icons/fa";
+import { RiMenuSearchLine } from "react-icons/ri";
+import { PiUserListBold } from "react-icons/pi";
+import { TbLogout2 } from "react-icons/tb";
+import { Tooltip } from "@chakra-ui/react"
+ 
+const Demo = () => {
+  return (
+    <Tooltip label="This is the tooltip content" openDelay={0} closeDelay={0}>
+    </Tooltip>
+  )
+}
 
 export function MenulateralHome() {
   const headerBgColor = useColorModeValue('header.light', 'header.dark');
@@ -9,24 +21,32 @@ export function MenulateralHome() {
       <>
         <Box className="menu-lateralHome" bg={headerBgColor}>
           <Link to="/">
-            <button className="btn-home">
-              Home
-            </button>
+              <Tooltip label={<span className='teste-span'>Home</span>} openDelay={0} closeDelay={0}>
+              <button className="btn-home">
+                <FaHome className='icone' />
+              </button>
+            </Tooltip>
           </Link>
           <Link to="/todosrecursos">
-            <button className="btn-todosRecursos">
-              Todos os Recursos
-            </button>
+            <Tooltip label={<span className='teste-span'>Todos <br /> os Recursos</span>} openDelay={0} closeDelay={0}>
+              <button className="btn-todosRecursos">
+                <RiMenuSearchLine className='icone' />
+              </button>
+            </Tooltip>
           </Link>
           <Link to="/meusrecursos">
-            <button className="btn-meusRecursos">
-              Meus recursos
-            </button>
+          <Tooltip label={<span className='teste-span'>Meus <br /> recursos</span>} openDelay={0} closeDelay={0}>
+              <button className="btn-meusRecursos">
+                <PiUserListBold className='icone' />
+              </button>
+            </Tooltip>
           </Link>
 
-          <button className="btn-voltarLogin">
-            Voltar Login
-          </button>
+          <Tooltip label={<span className='teste-span'>Logout</span>} openDelay={0} closeDelay={0}>
+            <button className="btn-voltarLogin">
+              <TbLogout2 className='icone' />
+            </button>
+          </Tooltip>
         </Box>
       </>
     )
